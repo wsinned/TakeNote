e2ebefe0a95b3f7ca6bd42ffe5ac124a487a9ca5
Dennis Woodruff <denniswoodruffuk@gmail.com>
2024-02-16 09:03:57 +0000

    Split args on space to make tests pass.

--- TakeNote.Cli/Parser.cs
+++ TakeNote.Cli/Parser.cs
@@ -3,6 +3,6 @@ namespace TakeNote.Cli;
 public class Parser {
 
     public static IEnumerable<string> Parse(string args) {
-        return args.Split(" ");
+        return args.Split(",");
     }
 }
